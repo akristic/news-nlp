77604399a19c4b993d30f617e97d123ce96854bd
Ante Kristic <ante.kristic81@gmail.com>
2020-05-14 12:36:15 +0200

    API, Sass, loaders, dependecies

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const AYLIENTextAPI = require('aylien_textapi');
 const app = express()
+
+
+let textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
 
 app.use(express.static('dist'))
 
@@ -10,14 +18,23 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+app.get('/nlp', function (req, res) {
+    textapi.sentiment({
+        'text': 'John is a very good football player!'
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+        }
+      });
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,30 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
+const AYLIENTextAPI = require('aylien_textapi');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             }
         ]
     },
@@ -18,6 +32,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
     ]
 }
